eb6482873357ae8f5b86649a8984e56a9b2a80c7
Tatchagon Koonkoei <o_k_t@hotmail.com>
2025-05-27 18:01:49 +0700

    wrap with RecoilRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { RecoilRoot } from "recoil"
+
 import { MantineProvider, createTheme } from "@mantine/core"
 import '@mantine/core/styles.css'
 
@@ -11,8 +13,10 @@ const theme = createTheme({})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider theme={theme}>
-      <App />
-    </MantineProvider>
+    <RecoilRoot>
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
+    </RecoilRoot>
   </StrictMode>,
 )
